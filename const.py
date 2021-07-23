9c6f3df4f2e52df76c6415da6fed72f5b66c460c
evanzhong <evanzhong@ucla.edu>
2021-07-23 16:35:28 -0700

    added FRAMES arrays to const.py

--- const.py
+++ const.py
@@ -3,4 +3,12 @@ import os
 DATA_OUTPUT_DIRECTORY = 'out'
 os.makedirs(DATA_OUTPUT_DIRECTORY, exist_ok=True)
 
+IMAGE_FILE_EXTENSION = 'jpg'
+
+MINION_2_DIRECTORY = 'minion_2_pics'
+MINION_2_FRAMES = sorted([f'{MINION_2_DIRECTORY}/{file_name}' for file_name in os.listdir(MINION_2_DIRECTORY) if file_name.endswith(IMAGE_FILE_EXTENSION)])
+
+MINION_3_DIRECTORY = 'minion_3_pics'
+MINION_3_FRAMES = sorted([f'{MINION_3_DIRECTORY}/{file_name}' for file_name in os.listdir(MINION_3_DIRECTORY) if file_name.endswith(IMAGE_FILE_EXTENSION)])
+
 print("const.py module loaded")
