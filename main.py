4bb96c6bbae0a37c925ee56123c14747254b712d
evanzhong <evanzhong@ucla.edu>
2021-08-02 21:40:26 -0700

    compare_two_images interface

--- main.py
+++ main.py
@@ -3,41 +3,6 @@ import const
 import sift
 import segmentation as seg
 
-m3_img0  = util.read_image(const.MINION_3_FRAMES[0])
-m3_img1  = util.read_image(const.MINION_3_FRAMES[1])
-
-m3_img0_keypoints, m3_img0_descriptors = sift.run_sift(m3_img0, num_features=3000)
-m3_img1_keypoints, m3_img1_descriptors = sift.run_sift(m3_img1, num_features=3000)
-
-m3_img0_img1_correspondences = sift.find_sift_correspondences(m3_img0_keypoints,
-                                                              m3_img0_descriptors,
-                                                              m3_img1_keypoints,
-                                                              m3_img1_descriptors,
-                                                              ratio=0.6)
-
-m3_img0_contours = seg.find_contours(m3_img0)
-m3_img1_contours = seg.find_contours(m3_img1)
-
-m3_img0_boxes = seg.merge_overlapping_bounding_boxes(seg.find_bounding_boxes(m3_img0_contours, padding=0))
-m3_img1_boxes = seg.merge_overlapping_bounding_boxes(seg.find_bounding_boxes(m3_img1_contours, padding=0))
-
-m3_img0_with_boxes = seg.draw_bounding_boxes(m3_img0,
-                                             bounding_boxes=m3_img0_boxes,
-                                             border_color=const.COLOR_GREEN,
-                                             border_width=2,
-                                             file_name='m3_img0_with_boxes')
-m3_img1_with_boxes = seg.draw_bounding_boxes(m3_img1,
-                                             bounding_boxes=m3_img1_boxes,
-                                             border_color=const.COLOR_GREEN,
-                                             border_width=2,
-                                             file_name='m3_img1_with_boxes')
-
-sift.plot_correspondences(image1=m3_img0_with_boxes,
-                          image2=m3_img1_with_boxes,
-                          correspondences=m3_img0_img1_correspondences,
-                          color=const.COLOR_RED,
-                          file_name='m3_img0_img1_corr_and_boxes')
-
 def get_box_containing_point(point, boxes):
 	point_x = point[0]
 	point_y = point[1]
@@ -74,22 +39,61 @@ def filter_correspondences_and_boxes(correspondences, boxes_0, boxes_1):
 
   return (filtered_correspondences, filtered_boxes_0, filtered_boxes_1)
 
-m3_img0_img1_correspondences_filtered, m3_img0_boxes_filtered, m3_img1_boxes_filtered = filter_correspondences_and_boxes(correspondences=m3_img0_img1_correspondences, boxes_0=m3_img0_boxes, boxes_1=m3_img1_boxes)
+def compare_two_images(img_0, img_1, num_sift_features, sift_correspondence_ratio, should_save_images = False):
+  img0_keypoints, img0_descriptors = sift.run_sift(img_0, num_features=num_sift_features)
+  img1_keypoints, img1_descriptors = sift.run_sift(img_1, num_features=num_sift_features)
 
-sift.plot_correspondences(image1=m3_img0_with_boxes,
-                          image2=m3_img1_with_boxes,
-                          correspondences=m3_img0_img1_correspondences_filtered,
-                          color=const.COLOR_RED,
-                          file_name='m3_img0_img1_correspondences_filtered')
-print(f'm3_img0_img1_correspondences length: {len(m3_img0_img1_correspondences)} m3_img0_img1_correspondences_filtered length: {len(m3_img0_img1_correspondences_filtered)}')
+  img0_img1_correspondences = sift.find_sift_correspondences(img0_keypoints,
+                                                             img0_descriptors,
+                                                             img1_keypoints,
+                                                             img1_descriptors,
+                                                             ratio=sift_correspondence_ratio)
 
-m3_img0_with_boxes_filtered = seg.draw_bounding_boxes(m3_img0, bounding_boxes=m3_img0_boxes_filtered, border_color=const.COLOR_GREEN, file_name='m3_img0_with_boxes_filtered')
-m3_img1_with_boxes_filtered = seg.draw_bounding_boxes(m3_img1, bounding_boxes=m3_img1_boxes_filtered, border_color=const.COLOR_GREEN, file_name='m3_img1_with_boxes_filtered')
-sift.plot_correspondences(
-  image1=m3_img0_with_boxes_filtered,
-  image2=m3_img1_with_boxes_filtered,
-  correspondences=m3_img0_img1_correspondences_filtered,
-  color=const.COLOR_RED,
-  file_name='m3_img0_img1_corr_AND_boxes_filtered'
-)
-print(f'check image m3_img0_img1_corr_AND_boxes_filtered that only boxes with corresopndences appear')
+  img0_contours = seg.find_contours(img_0)
+  img1_contours = seg.find_contours(img_1)
+  img0_boxes = seg.merge_overlapping_bounding_boxes(
+    bounding_boxes=seg.find_bounding_boxes(img0_contours, padding=0)
+  )
+  img1_boxes = seg.merge_overlapping_bounding_boxes(
+    bounding_boxes=seg.find_bounding_boxes(img1_contours, padding=0)
+  )
+
+  filtered_correspondences, filtered_boxes_0, filtered_boxes_1 = filter_correspondences_and_boxes(
+    correspondences=img0_img1_correspondences,
+    boxes_0=img0_boxes,
+    boxes_1=img1_boxes
+  )
+
+  if should_save_images:
+    img0_drawn_boxes = seg.draw_bounding_boxes(img_0, bounding_boxes=img0_boxes, border_color=const.COLOR_GREEN)
+    img1_drawn_boxes = seg.draw_bounding_boxes(img_1, bounding_boxes=img1_boxes, border_color=const.COLOR_GREEN)
+    sift.plot_correspondences(image1=img0_drawn_boxes,
+                              image2=img1_drawn_boxes,
+                              correspondences=img0_img1_correspondences,
+                              color=const.COLOR_RED,
+                              file_name='m3_img0_img1_corr_and_boxes')
+    sift.plot_correspondences(image1=img0_drawn_boxes,
+                              image2=img1_drawn_boxes,
+                              correspondences=filtered_correspondences,
+                              color=const.COLOR_RED,
+                              file_name='m3_img0_img1_corr_filtered_and_boxes')
+
+    img0_drawn_filtered_boxes = seg.draw_bounding_boxes(img_0, bounding_boxes=filtered_boxes_0, border_color=const.COLOR_GREEN)
+    img1_drawn_filtered_boxes = seg.draw_bounding_boxes(img_1, bounding_boxes=filtered_boxes_1, border_color=const.COLOR_GREEN)
+    sift.plot_correspondences(image1=img0_drawn_filtered_boxes,
+                              image2=img1_drawn_filtered_boxes,
+                              correspondences=img0_img1_correspondences,
+                              color=const.COLOR_RED,
+                              file_name='m3_img0_img1_corr_and_boxes_filtered')
+    sift.plot_correspondences(image1=img0_drawn_filtered_boxes,
+                              image2=img1_drawn_filtered_boxes,
+                              correspondences=filtered_correspondences,
+                              color=const.COLOR_RED,
+                              file_name='m3_img0_img1_corr_filtered_and_boxes_filtered')
+
+  return filtered_correspondences, filtered_boxes_0, filtered_boxes_1
+
+if __name__ == "__main__":
+  m3_img0  = util.read_image(const.MINION_3_FRAMES[0])
+  m3_img1  = util.read_image(const.MINION_3_FRAMES[1])
+  compare_two_images(m3_img0, m3_img1, num_sift_features=3000, sift_correspondence_ratio=0.6, should_save_images=True)
