7424fa6d3be833aaaf6b070aaf25612992c5da1b
Evan Zhong <evanzhong@ucla.edu>
2021-08-08 18:42:02 -0700

    Code motion refactor (#3)
    * move get_readable_correspondence to util module
    
    * move get_box_containing_point to util module
    
    * read_all_images interface
    
    * move get_box_area to utils module
    
    * move has_intersection, union, and remove_null_boxes to util
    
    * update bulk_carbon to accept margins parameter
    
    * refactor analyze_frames to take margins parameter
    
    * rename analyze_frames to track_particle_motion
    
    * ignore particle tracking runlogs
    
    * get_no_extension_filename util function
    
    * get_no_extension_filename also to remove /
    
    * lift should_save responsibility to track_particle_motion
    
    * const-ify FRAME_TO_ANALYZE

--- main.py
+++ main.py
@@ -3,24 +3,6 @@ import const
 import sift
 import segmentation as seg
 
-def get_box_containing_point(point, boxes):
-	point_x = point[0]
-	point_y = point[1]
-	for box in boxes:
-		box_x = box[0]
-		box_y = box[1]
-		box_width = box[2]
-		box_height = box[3]
-		point_is_in_box = (
-			point_x >= box_x and
-			point_x <= box_x + box_width and
-			point_y >= box_y and
-			point_y <= box_y + box_height
-		)
-		if point_is_in_box:
-			return box
-	return None
-
 def filter_correspondences_and_boxes(correspondences, boxes_0, boxes_1):
   filtered_correspondences = []
   filtered_boxes_0 = []
@@ -29,8 +11,8 @@ def filter_correspondences_and_boxes(correspondences, boxes_0, boxes_1):
     point_0 = correspondence[0]
     point_1 = correspondence[1]
     
-    box_0_containing_point = get_box_containing_point(point_0, boxes_0)
-    box_1_containing_point = get_box_containing_point(point_1, boxes_1)
+    box_0_containing_point = util.get_box_containing_point(point_0, boxes_0)
+    box_1_containing_point = util.get_box_containing_point(point_1, boxes_1)
     if (box_0_containing_point is not None and
         box_1_containing_point is not None and
         box_0_containing_point not in filtered_boxes_0 and
@@ -42,7 +24,7 @@ def filter_correspondences_and_boxes(correspondences, boxes_0, boxes_1):
 
   return (filtered_correspondences, filtered_boxes_0, filtered_boxes_1)
 
-def compare_two_images(img_0, img_1, num_sift_features, sift_correspondence_ratio, should_save_images = False):
+def compare_two_images(img_0, img_1, num_sift_features, sift_correspondence_ratio):
   img0_keypoints, img0_descriptors = sift.run_sift(img_0, num_features=num_sift_features)
   img1_keypoints, img1_descriptors = sift.run_sift(img_1, num_features=num_sift_features)
 
@@ -67,54 +49,37 @@ def compare_two_images(img_0, img_1, num_sift_features, sift_correspondence_rati
     boxes_1=img1_boxes
   )
 
-  if should_save_images:
-    img0_drawn_boxes = seg.draw_bounding_boxes(img_0, bounding_boxes=img0_boxes, border_color=const.COLOR_GREEN)
-    img1_drawn_boxes = seg.draw_bounding_boxes(img_1, bounding_boxes=img1_boxes, border_color=const.COLOR_GREEN)
-    sift.plot_correspondences(image1=img0_drawn_boxes,
-                              image2=img1_drawn_boxes,
-                              correspondences=img0_img1_correspondences,
-                              color=const.COLOR_RED,
-                              file_name='img0_img1_corr_and_boxes')
-    sift.plot_correspondences(image1=img0_drawn_boxes,
-                              image2=img1_drawn_boxes,
-                              correspondences=filtered_correspondences,
-                              color=const.COLOR_RED,
-                              file_name='img0_img1_corr_filtered_and_boxes')
-
-    img0_drawn_filtered_boxes = seg.draw_bounding_boxes(img_0, bounding_boxes=filtered_boxes_0, border_color=const.COLOR_GREEN)
-    img1_drawn_filtered_boxes = seg.draw_bounding_boxes(img_1, bounding_boxes=filtered_boxes_1, border_color=const.COLOR_GREEN)
-    sift.plot_correspondences(image1=img0_drawn_filtered_boxes,
-                              image2=img1_drawn_filtered_boxes,
-                              correspondences=img0_img1_correspondences,
-                              color=const.COLOR_RED,
-                              file_name='img0_img1_corr_and_boxes_filtered')
-    sift.plot_correspondences(image1=img0_drawn_filtered_boxes,
-                              image2=img1_drawn_filtered_boxes,
-                              correspondences=filtered_correspondences,
-                              color=const.COLOR_RED,
-                              file_name='img0_img1_corr_filtered_and_boxes_filtered')
-
   return filtered_correspondences, filtered_boxes_0, filtered_boxes_1
 
-def readable_correspondence(correspondence):
-  point_0 = correspondence[0]
-  point_1 = correspondence[1]
-  return ((round(point_0[0]), round(point_0[1])), (round(point_1[0]), round(point_1[1])))
-
-def analyze_frames(frames, num_sift_features, sift_correspondence_ratio, should_save_images=False):
+def track_particle_motion(frames, num_sift_features, sift_correspondence_ratio, should_save_comparisons=False, should_save_ROIs=False, margins=None):
   GLOBAL_MAP = {}
   GLOBAL_LIST = []
   for frame_n in range(len(frames)-1):
-    imgN  = util.read_image(frames[frame_n])
-    imgN_1  = util.read_image(frames[frame_n+1])
+    frame_n_filename = frames[frame_n]
+    frame_n_1_filename = frames[frame_n+1]
+    imgN  = util.read_image(frame_n_filename)
+    imgN_1  = util.read_image(frame_n_1_filename)
+    if margins != None:
+      imgN = get_image_center(image=imgN, margins=margins)
+      imgN_1 = get_image_center(image=imgN_1, margins=margins)
 
     corrs, boxes_N, boxes_N_1 = compare_two_images(
       imgN,
       imgN_1,
       num_sift_features=num_sift_features,
       sift_correspondence_ratio=sift_correspondence_ratio,
-      should_save_images=should_save_images
     )
+
+    if should_save_comparisons:
+      imgN_with_boxes = seg.draw_bounding_boxes(imgN, bounding_boxes=boxes_N, border_color=const.COLOR_GREEN)
+      img_N_1_with_boxes = seg.draw_bounding_boxes(imgN_1, bounding_boxes=boxes_N_1, border_color=const.COLOR_GREEN)
+      sift.plot_correspondences(
+        image1=imgN_with_boxes,
+        image2=img_N_1_with_boxes,
+        correspondences=corrs,
+        color=const.COLOR_RED,
+        file_name=f'{util.get_no_extension_filename(frame_n_filename)}_to_{util.get_no_extension_filename(frame_n_1_filename)}'
+      )
 
     # Initialize the GLOBAL objects with frame_0 information
     if frame_n == 0:
@@ -135,8 +100,8 @@ def analyze_frames(frames, num_sift_features, sift_correspondence_ratio, should_
       imgN_1_ROI = seg.crop_image(imgN_1, box_N_1)
       imgN_ROI_area = seg.get_non_zero_pixel_area(imgN_ROI)
       imgN_1_ROI_area = seg.get_non_zero_pixel_area(imgN_1_ROI)
-      print(f'({imgN_ROI_area},{imgN_1_ROI_area}) img{frame_n}_{box_N} to img{frame_n+1}_{box_N_1} via {readable_correspondence(corr)}')
-      if should_save_images: util.write_image(imgN_ROI, f'img{frame_n}_{box_N}')
+      print(f'({imgN_ROI_area},{imgN_1_ROI_area}) img{frame_n}_{box_N} to img{frame_n+1}_{box_N_1} via {util.get_readable_correspondence(corr)}')
+      if should_save_ROIs: util.write_image(imgN_ROI, f'img{frame_n}_{box_N}')
 
       list_index = -1
       if box_N in GLOBAL_MAP:
@@ -152,10 +117,13 @@ def analyze_frames(frames, num_sift_features, sift_correspondence_ratio, should_
   print(GLOBAL_LIST)
   return GLOBAL_LIST
 
-def bulk_carbon(frames):
+def bulk_carbon(frames, margins=None):
   frame_to_carbon_map = {}
   for frame in frames:
     frame_image = util.read_image(frame)
+    if margins != None:
+      frame_image = get_image_center(image=frame_image, margins=margins)
+
     contours = seg.find_contours(image=frame_image)
     boxes = seg.merge_overlapping_bounding_boxes(
       bounding_boxes=seg.find_bounding_boxes(contours=contours, padding=0)
@@ -174,12 +142,25 @@ def bulk_carbon(frames):
   print(frame_to_carbon_map)
   return frame_to_carbon_map
 
+def get_image_center(image, margins):
+  image_shape = image.shape
+  margin_top, margin_right, margin_bot, margin_left = margins
+  max_y, max_x, channels = image_shape
+  center_box = (margin_left, margin_top, max_x-(margin_right*2), max_y-(margin_bot*2))
+  cropped_image = seg.crop_image(image=image, box=center_box)
+  return cropped_image
+
 if __name__ == "__main__":
-  analyze_frames(
-    frames=const.MINION_3_FRAMES,
+  MARGINS_TO_USE = (300, 275, 400, 175)
+  FRAME_TO_ANALYZE = const.MINION_3_FRAMES
+
+  track_particle_motion(
+    frames=FRAME_TO_ANALYZE,
     num_sift_features=3000,
     sift_correspondence_ratio=0.6,
-    should_save_images=False
+    should_save_comparisons=False,
+    should_save_ROIs=False,
+    margins=MARGINS_TO_USE
   )
 
-  bulk_carbon(frames=const.MINION_3_FRAMES)
+  bulk_carbon(frames=FRAME_TO_ANALYZE, margins=MARGINS_TO_USE)
--- util.py
+++ util.py
@@ -2,10 +2,61 @@ import const
 
 import cv2
 
+def get_no_extension_filename(full_file_name):
+	no_extension_filename = full_file_name.split('.')[0]
+	no_extension_filename = no_extension_filename.replace('/', '_')
+	return no_extension_filename
+
 def read_image(file_name):
   return cv2.imread(file_name)
 
+def read_all_images(file_names):
+	images = [read_image(file_name) for file_name in file_names]
+	return list(zip(images, file_names))
+
 def write_image(image, file_output_name):
   return cv2.imwrite(f'{const.DATA_OUTPUT_DIRECTORY}/{file_output_name}.{const.IMAGE_FILE_EXTENSION}', image)
+
+def get_readable_correspondence(correspondence):
+  point_0, point_1 = correspondence
+  return ((round(point_0[0]), round(point_0[1])), (round(point_1[0]), round(point_1[1])))
+
+def get_box_area(box):
+	x, y, width, height = box
+	return width * height
+
+# Adapted from https://stackoverflow.com/questions/46260892/finding-the-union-of-multiple-overlapping-rectangles-opencv-python/57546435
+def has_intersection(box_1,box_2):
+	x = max(box_1[0], box_2[0])
+	y = max(box_1[1], box_2[1])
+	width = min(box_1[0]+box_1[2], box_2[0]+box_2[2]) - x
+	height = min(box_1[1]+box_1[3], box_2[1]+box_2[3]) - y
+	if width < 0 or height < 0: return False
+	else: return get_box_area((x, y, width, height)) > 0
+
+# Adapted from https://stackoverflow.com/questions/46260892/finding-the-union-of-multiple-overlapping-rectangles-opencv-python/57546435
+def union(box_1,box_2):
+  x = min(box_1[0], box_2[0])
+  y = min(box_1[1], box_2[1])
+  width = max(box_1[0]+box_1[2], box_2[0]+box_2[2]) - x
+  height = max(box_1[1]+box_1[3], box_2[1]+box_2[3]) - y
+  return (x, y, width, height)
+
+def remove_null_boxes(boxes):
+	return [box for box in boxes if get_box_area(box) != 0]
+
+def get_box_containing_point(point, boxes):
+	point_x, point_y = point
+	for box in boxes:
+		box_x, box_y, box_width, box_height = box
+		point_is_in_box = (
+			point_x >= box_x and
+			point_x <= box_x + box_width and
+			point_y >= box_y and
+			point_y <= box_y + box_height
+		)
+		if point_is_in_box:
+			return box
+	return None
 
 print('util.py module loaded')
